8c772683c4293d980f3ad0de9c9f1c881905dfcd
HOCgaming <harveycash@outlook.com>
2017-03-05 15:05:03 +0000

    Initial Project

---
+++ One More Dimension/Assets/Scripts/GrabbableObject.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrabbableObject : MonoBehaviour {
+    protected bool debug = true;
+    private const int VEL_ARRAY_SIZE = 3;
+
+	public virtual void grabbed (ViveController controller) {
+        //if(debug) { Debug.Log(gameObject.name + " has been grabbed."); }
+        gameObject.transform.parent = controller.transform;
+        gameObject.GetComponent<Rigidbody>().useGravity = false;
+        gameObject.GetComponent<Rigidbody>().isKinematic = true;
+
+        //deal with passing from hand to hand
+        if (grabbedBy != null && grabbedBy.gameObject != controller.gameObject && grabbedBy.GetComponent<ViveController>() != null) {
+            grabbedBy.GetComponent<ViveController>().forgetGrabbed();
+        }
+        grabbedBy = controller.transform;
+    }
+    public virtual void released (ViveController controller) {
+        //if (debug) { Debug.Log(gameObject.name + " has been released."); }
+        gameObject.transform.parent = null;
+        gameObject.GetComponent<Rigidbody>().isKinematic = false;
+        gameObject.GetComponent<Rigidbody>().useGravity = true;
+        grabbedBy = null;
+
+        gameObject.GetComponent<Rigidbody>().velocity = getReleaseVelocity();
+    }
+
+    //for throwing, it's better to base velocity on the controller - rather than
+    //the object itself.
+    protected Transform grabbedBy = null;
+    private Vector3[] positionArray = new Vector3[VEL_ARRAY_SIZE + 1];
+    private Vector3[] velocityArray = new Vector3[VEL_ARRAY_SIZE];
+    //start off with zero vectors, avoid null errors
+    private void initialiseArrays() {
+        for (int i = 0; i < positionArray.Length; i++) { positionArray[i] = Vector3.zero; }
+        for (int i = 0; i < velocityArray.Length; i++) { velocityArray[i] = Vector3.zero; }
+    }
+    //take last few frames and determine release velocity.
+    private void measureReleaseVelocity() {
+        //which transform do we pay attention to?
+        if (grabbedBy != null) {
+            //shift all vectors in the array up, add current vector to the end
+            for (int i = 0; i < positionArray.Length - 1; i++) { positionArray[i] = positionArray[i + 1]; }
+            positionArray[positionArray.Length - 1] = grabbedBy.position;
+            for (int i = 0; i < velocityArray.Length - 1; i++) {
+                velocityArray[i] = velocityArray[i + 1];
+            }
+            Vector3 deltaPos = positionArray[positionArray.Length - 1] - positionArray[positionArray.Length - 2];
+            velocityArray[velocityArray.Length - 1] = deltaPos / Time.deltaTime;
+        }
+    }
+
+    private Vector3 getReleaseVelocity() {
+        //averaging method
+        float averageX = 0f, averageY = 0f, averageZ = 0f;
+        for(int i = 0; i < velocityArray.Length; i++) {
+            averageX += velocityArray[i].x;
+            averageY += velocityArray[i].y;
+            averageZ += velocityArray[i].z;
+        }
+        averageX = averageX / velocityArray.Length;
+        averageY = averageY / velocityArray.Length;
+        averageZ = averageZ / velocityArray.Length;
+
+        Vector3 averageVelocity = new Vector3(averageX, averageY, averageZ);
+        return averageVelocity;
+    }
+
+    public virtual void activate () {
+    }
+
+    //~~~~~~~~~~~~~~~~~~~~ METHOD CALLS ~~~~~~~~~~~~~~~~~~~~//
+
+    void Start() {
+        initialiseArrays();
+    }
+
+    void Update() {
+        measureReleaseVelocity();
+    }
+
+}
---
+++ One More Dimension/Assets/Scripts/ViveController.cs
@@ -0,0 +1,132 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ViveController : MonoBehaviour {
+
+    //~~~~~~~~~~~~~~~~~~~~ KEYBINDINGS ~~~~~~~~~~~~~~~~~~~~//
+
+    private enum Action { GRAB, ACTIVATE, MENU }
+    private enum Input { CLICK, LIFT, HOLD }
+    private bool getInput (Action action, Input press) {
+        switch (press) {
+            case Input.CLICK:
+                return controller.GetPressDown(getViveKey(action));
+            case Input.LIFT:
+                return controller.GetPressUp(getViveKey(action));
+            case Input.HOLD:
+                return controller.GetPress(getViveKey(action));
+                
+            default:
+                return false;
+        }
+    }
+    //returns the requested SteamVR ButtonMask.
+    //EDIT KEYBINDINGS HERE
+    private ulong getViveKey(Action action) {
+        switch (action) {
+            case Action.GRAB:
+                return SteamVR_Controller.ButtonMask.Trigger;
+            case Action.ACTIVATE:
+                return SteamVR_Controller.ButtonMask.Touchpad;
+            case Action.MENU:
+                return SteamVR_Controller.ButtonMask.ApplicationMenu;
+
+            //THIS SHOULD NEVER BE RETURNED.
+            default:
+                return SteamVR_Controller.ButtonMask.Trigger;
+        }
+    }
+
+    //~~~~~~~~~~~~~~~~~~~~ INITIALISATION ~~~~~~~~~~~~~~~~~~~~//
+
+    private SteamVR_TrackedObject trackedObj;
+    private SteamVR_Controller.Device controller { get { return SteamVR_Controller.Input((int)trackedObj.index); } }
+    private void controllerInit() {
+        trackedObj = GetComponent<SteamVR_TrackedObject>();
+        myModel = gameObject.transform.GetChild(0);
+
+        //give ourselves some trigger colliders
+        SphereCollider triggerCollider = gameObject.AddComponent<SphereCollider>();
+        triggerCollider.isTrigger = true;
+        triggerCollider.radius = 0.08f;
+        triggerCollider.center = new Vector3(0f, -0.05f, 0.03f);        
+    }    
+
+    //~~~~~~~~~~~~~~~~~~~~ GRABBING ~~~~~~~~~~~~~~~~~~~~//
+
+    private const float GRAB_TOGGLE_TIME = 0.3f;
+    private bool releaseOnNextLift = true;
+
+    private GrabbableObject couldGrab, haveGrabbed;
+    void OnTriggerEnter (Collider collider) {
+        if (collider.gameObject.GetComponent<GrabbableObject>() != null) {
+            SteamVR_Controller.Input((int)trackedObj.index).TriggerHapticPulse(500);
+            couldGrab = collider.gameObject.GetComponent<GrabbableObject>();
+        }
+    }
+    void OnTriggerExit (Collider collider) {
+        if (couldGrab != null && collider.gameObject == couldGrab.gameObject) {
+            couldGrab = null;
+        }
+    }	
+    //called by an object when it is grabbed by something new
+    public void forgetGrabbed() {
+        haveGrabbed = null;
+        releaseOnNextLift = true;        
+    }
+
+    private void checkButtonInputs () {
+        //if already grabbing something in toggle grab mode, release on the following lift
+        if (getInput(Action.GRAB, Input.CLICK) && haveGrabbed != null && !releaseOnNextLift) {
+            releaseOnNextLift = true;
+        }
+        //grab (only if not holding something already)
+        if (getInput(Action.GRAB, Input.CLICK) && couldGrab != null && haveGrabbed == null) {
+            StartCoroutine(waitForGrabToggle());
+            haveGrabbed = couldGrab;
+            haveGrabbed.grabbed(this);
+            SteamVR_Controller.Input((int)trackedObj.index).TriggerHapticPulse(1000);
+        }
+        //release, if the grab mode allows
+        if (getInput(Action.GRAB, Input.LIFT) && haveGrabbed != null && releaseOnNextLift) {
+            haveGrabbed.released(this);
+            haveGrabbed = null;
+        }
+
+        //activate
+        if (getInput(Action.ACTIVATE, Input.CLICK) && haveGrabbed != null) {
+            haveGrabbed.activate();
+            SteamVR_Controller.Input((int)trackedObj.index).TriggerHapticPulse(3000);
+        }
+    }
+
+    //if I grab and hold, assume that I want to throw!
+    private IEnumerator waitForGrabToggle() {
+        releaseOnNextLift = false;
+        yield return new WaitForSeconds(GRAB_TOGGLE_TIME);
+        if (haveGrabbed != null) { releaseOnNextLift = true; }
+    }
+
+    //~~~~~~~~~~~~~~~~~~~~ TOOL SPECIFIC METHODS ~~~~~~~~~~~~~~~~~~~~//
+
+    //hides the controller and its collider
+    private Transform myModel;
+    public void setVisible(bool visibility) {
+        myModel.gameObject.SetActive(visibility);
+        //gameObject.GetComponent<SphereCollider>().enabled = visibility;
+    }
+
+
+    //~~~~~~~~~~~~~~~~~~~~ METHOD CALLS ~~~~~~~~~~~~~~~~~~~~//
+
+    void Start() {
+        controllerInit();
+    }
+
+    void Update () {
+        checkButtonInputs();
+	}
+
+
+}
